cab418f46c4e9ae22d3111f5bad809566a4781fd
Ateran7595 <alexteran795@gmail.com>
2025-02-14 10:56:36 -0700

    New Project Created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+     <h1>Hallooo</h1>
+     <Button>IT WORKS!</Button>
+    </>
+  )
+}
+
+export default App
