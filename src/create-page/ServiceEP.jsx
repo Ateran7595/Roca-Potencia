b1f0278cf31999825f7a06a033de6f990b8b657b
Ateran7595 <alexteran795@gmail.com>
2025-02-19 21:24:21 -0700

    added carousel

--- src/create-page/ServiceEP.jsx
+++ src/create-page/ServiceEP.jsx
@@ -1,13 +1,9 @@
 import { Button } from '@/components/ui/button'
 import Header from './Header'
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "@/components/ui/carousel"
-  import Autoplay from "embla-carousel-autoplay"
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import { Autoplay ,Pagination, Navigation } from 'swiper/modules';
 
 function ServiceEP() {
   return (
@@ -23,7 +19,7 @@ function ServiceEP() {
         </div>
       </div>
       <div className="mt-[200px] text-xl font-roboto text-center font-semibold animate">
-        <div data-aos="fade-zoom-in"
+        <div className='text-[30px]' data-aos="fade-zoom-in"
             data-aos-easing="ease-in-back"
             data-aos-offset="0">
             <h1 className="text-[40px] font-bold">Roca & Potencia El Paso, TX</h1>
@@ -34,16 +30,27 @@ function ServiceEP() {
             <Button className="text-[22px] font-bold h-[50px] rounded-[100px] border-2 border-black cursor-pointer">Mapa</Button>
         </div>
       </div>
-        <Carousel plugins={[Autoplay({ delay: 2000, }), ]} className="flex w-[80%] m-auto items-center justify-center" >
-            <CarouselContent>
-                <CarouselItem>...</CarouselItem>
-                <CarouselItem>...</CarouselItem>
-                <CarouselItem>...</CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-        </Carousel>
-        
+      <div>
+        <h1>Clases Para Ninos de edades 3+</h1>
+      </div>
+      <Swiper className='w-[600px]' 
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]} >
+        <SwiperSlide><img src="/roca1.jpg" alt="1" className='w-[100%] h-[100%]'  /></SwiperSlide>
+        <SwiperSlide><img src="/roca2.jpg" alt="2" className='w-[100%] h-[100%]' /></SwiperSlide>
+        <SwiperSlide><img src="/rocaEP.jpg" alt="ep" className='w-[100%] h-[100%]' /></SwiperSlide>
+        <SwiperSlide><img src="/roca4.jpg" alt="4" className='w-[100%] h-[100%]' /></SwiperSlide>
+      </Swiper>
     </div>
   )
 }
