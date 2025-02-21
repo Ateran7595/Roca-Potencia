a8178050c30d2553fcb4a8d069155186c94daa61
Ateran7595 <alexteran795@gmail.com>
2025-02-20 22:30:26 -0700

    Made some advancements in serviceep page

--- src/create-page/Header.jsx
+++ src/create-page/Header.jsx
@@ -4,9 +4,16 @@ import { useLocation } from 'react-router-dom';
 function Header() {
   const location = useLocation(); // Get the current page URL
   const isLandingPage = location.pathname === '/'; // Check if we are on the landing page
+  const isServicePage = location.pathname === '/serviceep';
+
+  const headerBackground = isLandingPage
+  ? 'bg-[linear-gradient(-20deg,#616161_0%,#9bc5c3_100%)]'
+  : isServicePage
+  ? 'bg-[linear-gradient(-225deg,#CBBACC_0%,#2580B3_100%)]'
+  : 'bg-gray-800'; 
 
   return (
-    <div className='flex justify-between items-center font-roboto bg-[linear-gradient(-20deg,#616161_0%,#9bc5c3_100%)] p-6'>
+    <div className={`flex justify-between items-center font-roboto ${headerBackground} p-6`}>
       <a href="/">
         <div className='flex items-center gap-2 transition-transform hover:scale-105 cursor-pointer' data-aos="fade-zoom-in">
           <img src="/RPlogo.png" alt="logo" className='w-[90px] h-[90px]' />
--- src/create-page/ServiceEP.jsx
+++ src/create-page/ServiceEP.jsx
@@ -17,7 +17,7 @@ function ServiceEP() {
   return (
     <div className="bg-gradient-to-t from-[#dfe9f3] to-white ">
       <Header />
-      <div className="bg-[linear-gradient(-20deg,#616161_0%,#9bc5c3_100%)] flex flex-col items-center justify-center w-full p-5 h-[900px] relative">
+      <div className="bg-[linear-gradient(-225deg,_#CBBACC_0%,_#2580B3_100%)] flex flex-col items-center justify-center w-full p-5 h-[900px] relative">
         <div className="relative w-full flex items-center justify-center">
           <img src="/rocaEP.jpg" alt="roca5" className="w-[1300px] rounded-[50px] brightness-70 relative top-[150px] object-cover" data-aos="fade-up" />
           <div className="absolute text-[100px] text-white bottom-[610px] text-center" data-aos="fade-down">
@@ -33,12 +33,14 @@ function ServiceEP() {
           <div className="border-b-3 w-[40%] m-auto mt-2 mb-2"></div>
           <p>915 Delta Drive, El Paso, Texas</p>
           <p>11:00am - 1:30pm</p>
-          <Button className="text-[22px] mt-3 font-bold h-[50px] rounded-[100px] border-2 border-black cursor-pointer">Mapa</Button>
+          <a href="https://www.google.com/maps/place/Templo+La+Potenica+De+Dios/@31.7271681,-106.4935073,14z/data=!4m6!3m5!1s0x86e75904d111d355:0x88355d21d23276b9!8m2!3d31.7573792!4d-106.4793629!16s%2Fg%2F11c1sk1w8n?entry=ttu&g_ep=EgoyMDI1MDIxOC4wIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D" target='_blank'>
+            <Button className="text-[22px] mt-3 font-bold h-[50px] rounded-[100px] border-2 border-black cursor-pointer transition-transform hover:scale-105 hover:opacity-40 shadow-2xl" >Mapa</Button>
+          </a>
         </div>
       </div>
 
       <div className='mt-15 mb-4 w-[30%] m-auto outline-3 rounded-xl' data-aos="fade-down">
-        <h1 className='text-[30px] font-bold font-roboto text-center '>Clases Para Niños Desde Edades 3-10+</h1>
+        <h1 className='text-[30px] font-bold font-roboto text-center shadow-2xl'>Clases Para Niños Desde Edades 3-10+</h1>
       </div>
 
       {/* Add a unique key to force rerender */}
@@ -56,7 +58,7 @@ function ServiceEP() {
           <img src="ep5.jpg" alt="mountain" className="brightness-75 max-w-none object-cover h-[500px] rounded-2xl" />
         </div>
       </div>
-      <div className='bg-[linear-gradient(-20deg,#616161_0%,#9bc5c3_100%)] rounded-t-2xl mt-10 text-white p-4'>
+      <div className='bg-[linear-gradient(-225deg,_#CBBACC_0%,_#2580B3_100%)] rounded-t-2xl mt-10 text-white p-4'>
         <h1 className='text-center text-[35px] font-bold outline w-[40%] m-auto font-roboto'>Tambien Se Parte De Nuestro Grupo De Jovenes!</h1>
         
       </div>
