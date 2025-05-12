3b58c489c91927fbf1edb21d81534c5ee7867e37
Ateran7595 <alexteran795@gmail.com>
2025-05-12 14:22:15 -0600

    New page transition

--- src/create-page/PageWrapper.jsx
+++ src/create-page/PageWrapper.jsx
@@ -5,7 +5,7 @@ const PageWrapper = ({ children }) => (
     initial={{ opacity: 0, y: 30 }}
     animate={{ opacity: 1, y: 0 }}
     exit={{ opacity: 0, y: -30 }}
-    transition={{ duration: 2 }}
+    transition={{ duration: 1 }}
   >
     {children}
   </motion.div>
